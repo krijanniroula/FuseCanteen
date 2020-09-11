10e876f415a4a82b79ed70c1717ac5943ecbfe16
unknown <krijanniraula@gmail.com>
2020-09-11 18:42:42 +0545

    initial commit

---
+++ src/test/java/com/fusemachines/fusecanteen/FuseCanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusemachines.fusecanteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuseCanteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
